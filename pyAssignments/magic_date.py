174e19712d31726b7a8d21822d3d71a20f6e3350
dnmcginn57 <nicholasmcginn57@yahoo.com>
2019-09-09 13:28:52 -0500

    magic_date done

---
+++ pyAssignments/magic_date.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+"""
+David McGinn
+Program 1 - 9/11/19
+This program determines whether a certain date is magic
+(month * day = year)
+"""
+
+import sys, os
+
+"""
+    Accepts:
+        int m - month
+        int d - day
+        int y - year
+    Returns:
+        tuple - (boolean, errormsg)
+    ensures date is an actual, possible date(sort of)    
+"""
+def verify(m,d,y):
+    if m > 12 or m < 1:
+        return (False,"invalid month")
+    elif d > 31 or d <1:
+        return (False,"invalid day")
+    elif y > 99 or y < 0:
+        return (False,"invalid year")
+    return (True, ";^)")
+
+"""
+    Accepts: 
+        int m - month
+        int d - day
+        int y - year
+    Returns:
+        none
+    Determines wether the date is magic by
+    comparing m * d to y, printing the result
+"""
+def is_Magic(m,d,y):
+    mult = m*d
+    if mult is y:
+        print (str(m) + "/" + str(d) + "/" + str(y) + " is magic because ")
+        print (str(m) + " * " + str(d) + " = " + str(y))
+    else:
+        print (str(m) + "/" + str(d) + "/" + str(y) + " is not magic because ")
+        print (str(m) + " * " + str(d) + " != " + str(y))
+
+"""
+    Entry point function
+"""
+if __name__ == "__main__":
+    date = input("input date (mm/dd/yy)\n")
+    
+    date_List = date.split('/')
+    
+    m = int(date_List[0])
+    d = int(date_List[1])
+    y = int(date_List[2])
+
+    is_Valid = verify(m,d,y)
+    if is_Valid[0]:
+        is_Magic(m,d,y)
+    else:
+        print(is_Valid[1])
+
+
+
+
